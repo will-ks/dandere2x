a15626202901360573f7ec90fd529b1fa250f72e
will-ks <will@willks.com>
2023-03-27 19:02:41 +0200

    wip

--- server/src/main.ts
+++ server/src/main.ts
@@ -97,9 +97,9 @@ const messages: string[] = [];
       "-ws",
       "./workspace/",
       "-i",
-      Path.join(__dirname, "public/output.mp4"),
+      "output.mp4",
       "-o",
-      Path.join(__dirname, "public/upscaled.mkv"),
+      "upscaled.mkv",
     ],
     {
       cwd: Path.join(__dirname, "../dandere2x/dandere2x/src/"),
